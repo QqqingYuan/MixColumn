44932451149fef370fa198b49b3b3152a95ba620
qingyuan <ae5656ln@163.com>
2016-10-10 21:50:06 +0800

    transform single column

---
+++ src/main/java/cn/kejso/Mix/Mixture.java
@@ -0,0 +1,49 @@
+package cn.kejso.Mix;
+
+import java.util.List;
+
+import cn.kejso.AbstractTransform;
+
+public class Mixture {
+	
+	private String table;
+	private String new_table;
+	private Field originalFields;
+	private Field mappingFields;
+	//transform
+	private AbstractTransform  transform;
+	
+	public String getTable() {
+		return table;
+	}
+	public void setTable(String table) {
+		this.table = table;
+	}
+	public Field getOriginalFields() {
+		return originalFields;
+	}
+	public void setOriginalFields(Field originalFields) {
+		this.originalFields = originalFields;
+	}
+	public Field getMappingFields() {
+		return mappingFields;
+	}
+	public void setMappingFields(Field mappingFields) {
+		this.mappingFields = mappingFields;
+	}
+	public String getNew_table() {
+		return new_table;
+	}
+	public void setNew_table(String new_table) {
+		this.new_table = new_table;
+	}
+	public AbstractTransform getTransform() {
+		return transform;
+	}
+	public void setTransform(AbstractTransform transform) {
+		this.transform = transform;
+	}
+	
+	
+
+}
